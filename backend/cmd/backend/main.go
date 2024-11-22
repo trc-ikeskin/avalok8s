a1df7420d2c84a0a5ed51f3f2561f975daf090f6
trc-ikeskin <ikeskin@tecracer.de>
2024-11-22 20:53:25 +0100

    add(k8s-client): storage "engine" for k8s clientset add(k8s-client): clientset creation helper add(rest-api): middleware for storage "engine" add(rest-api): post client route add(rest-api): get nodes for client route

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -2,81 +2,156 @@ package main
 
 import (
 	"context"
-	"flag"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
-	"os"
-	"path/filepath"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
-func main() {
+type RawKubeConfig struct {
+	Name                     string           `json:"name"`
+	Server                   string           `json:"server"`
+	CertificateAuthorityData []byte           `json:"ca-data,omitempty"`
+	Command                  string           `json:"command"`
+	Args                     []string         `json:"args"`
+	Env                      []api.ExecEnvVar `json:"env,omitempty"`
+}
 
-	namespace := parseFlags()
+// Storage Engine for Clients
 
-	/*
-		filepath.Join() constructs a file path and homeDir() retrieve
-		the current user's home directory ~/.kube/config is the default
-		location.
-	*/
-	kubeconfig := filepath.Join(
-		homeDir(), ".kube", "config",
-	)
+type KubeClientStore struct {
+	data map[string]*kubernetes.Clientset
+}
 
-	/*
-		clientcmd.BuildConfigFromFlags() reads the kubeconfig file to build
-		a configuration object (config) for connecting to the Kubernetes API.
-		The empty string "" indicates that no custom API server address is provided.
-	*/
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		panic(fmt.Errorf("failed to load kubeconfig: %v", err))
+func NewKubeClientStore() (*KubeClientStore, error) {
+	return &KubeClientStore{
+		data: make(map[string]*kubernetes.Clientset),
+	}, nil
+}
+
+func (k *KubeClientStore) Set(key string, value *kubernetes.Clientset) error {
+	k.data[key] = value
+	return nil
+}
+
+func (k *KubeClientStore) Get(key string) (*kubernetes.Clientset, error) {
+	if _, ok := k.data[key]; !ok {
+		return nil, fmt.Errorf("clientset with key %s not found!", key)
 	}
 
-	/*
-		kubernetes.NewForConfig(config) creates a clientset, which is an object that
-		contains methods for interacting with various Kubernetes resources (e.g., pods,
-		services, deployments, etc.).
-	*/
+	return k.data[key], nil
+}
+
+// Storage Middleware
+
+func KubeClientStoreMiddleware(kcs *KubeClientStore) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("kubeClientStore", kcs)
+		c.Next()
+	}
+}
+
+// ClientSet Creation Helper
+
+func CreateClientSet(apiServer string, execCommand string, execArgs []string, execEnv []api.ExecEnvVar, caData []byte) (*kubernetes.Clientset, error) {
+	// Validate CA data
+	if len(caData) > 0 {
+		block, _ := pem.Decode(caData)
+		if block == nil {
+			return nil, fmt.Errorf("invalid CA data: could not parse PEM")
+		}
+		if _, err := x509.ParseCertificate(block.Bytes); err != nil {
+			return nil, fmt.Errorf("invalid CA data: %v", err)
+		}
+	}
+
+	config := &rest.Config{
+		Host: apiServer,
+		TLSClientConfig: rest.TLSClientConfig{
+			CAData: caData,
+		},
+		ExecProvider: &api.ExecConfig{
+			Command:         execCommand,
+			Args:            execArgs,
+			Env:             execEnv,
+			APIVersion:      "client.authentication.k8s.io/v1beta1",
+			InteractiveMode: api.NeverExecInteractiveMode,
+		},
+	}
+
+	// Build the Clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(fmt.Errorf("failed to create clientset: %v", err))
+		return nil, fmt.Errorf("failed to create Kubernetes Clientset: %v", err)
 	}
 
-	/*
-		List
-	*/
-	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+	return clientset, nil
+}
+
+// REST API
+
+func GetNodes(c *gin.Context) {
+	id := c.Param("id")
+
+	kcs, exists := c.Get("kubeClientStore")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not get datastore"})
+		return
+	}
+	kubeClientStore := kcs.(*KubeClientStore)
+
+	kubeClient, err := kubeClientStore.Get(id)
 	if err != nil {
-		panic(fmt.Errorf("failed to list pods in namespace '%s': %v", namespace, err))
+		panic(fmt.Errorf("failed to get KubeClient: %v", err))
 	}
 
-	fmt.Printf("Pods in namespace '%s':\n", namespace)
-	for _, pod := range pods.Items {
-		fmt.Printf("%s\n", pod.Name)
+	nodes, err := kubeClient.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
+
+	c.IndentedJSON(http.StatusOK, &nodes)
 }
 
-// -- Helper Functions
+func PostKubeClient(c *gin.Context) {
+	var newRawKubeConfig RawKubeConfig
 
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
+	if err := c.BindJSON(&newRawKubeConfig); err != nil {
+		return
 	}
-	return os.Getenv("USERPROFILE") // if you run on Windows
+
+	clientset, err := CreateClientSet(newRawKubeConfig.Server, newRawKubeConfig.Command, newRawKubeConfig.Args, newRawKubeConfig.Env, newRawKubeConfig.CertificateAuthorityData)
+	if err != nil {
+		panic(fmt.Errorf("failed to create Kubernetes clientset: %v", err))
+	}
+
+	kcs, exists := c.Get("kubeClientStore")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not get datastore"})
+		return
+	}
+	kubeClientStore := kcs.(*KubeClientStore)
+
+	kubeClientStore.Set(newRawKubeConfig.Name, clientset)
 }
 
-func parseFlags() string {
-
-	namespace := flag.String("namespace", "default", "The namespace to list pods from")
-	ns := flag.String("n", "default", "Alias for --namespace")
-	flag.Parse()
-
-	if *ns != "default" {
-		return *ns
+func main() {
+	kubeClientStore, err := NewKubeClientStore()
+	if err != nil {
+		panic(fmt.Errorf("failed to create Store for KubeClients: %v", err))
 	}
 
-	return *namespace
+	router := gin.Default()
+	router.Use(KubeClientStoreMiddleware(kubeClientStore))
+
+	router.POST("/clients", PostKubeClient)
+	router.GET("/client/:id/nodes", GetNodes)
+
+	router.Run(":3000")
 }
